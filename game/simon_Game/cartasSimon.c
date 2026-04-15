80208088958397ad7373afc70b88cf8262fae574
Giovanni Carvalho <111886876+GiovanniMQC@users.noreply.github.com>
2026-04-15 17:06:09 +0100

    Inicializando funções base e adicionado .gitignore para não rastrear a pasta do VS Code

--- game/simon_Game/cartasSimon.c
+++ game/simon_Game/cartasSimon.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include "cartasSimon.h"
 
+//INTERFACE: Recebe uma carta e le o seu valor, printa o valor correspondente no terminal
+void print_valor(struct carta c) 
+{
+    switch (c.valor) 
+    {
+        case 1:  printf(" A   "); break;
+        case 10: printf(" 10  "); break; 
+        case 11: printf(" J   "); break;
+        case 12: printf(" Q   "); break;
+        case 13: printf(" K   "); break;
+        default: printf(" %d   ", c.valor); break;
+    }  
+}
+
 void print_naipe(struct carta c)
 {
     switch (c.naipe)
--- game/simon_Game/main.c
+++ game/simon_Game/main.c
@@ -5,5 +5,14 @@
 #include <locale.h>
 #include "cartasSimon.h"
 #include "logicaSimon.h"
+#include "cartasSimon.c"
+#include "logicaSimon.c"
 
 struct carta baralho[52];
+
+int main(void)
+{
+    print_carta(baralho[10]);
+    print_naipe(baralho[10]);
+    return 0;
+}
