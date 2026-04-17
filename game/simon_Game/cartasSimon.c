c14dba9c1cad93ea00d055c6de05a3c5783788ce
João Marchon <b0mb4j40@gmail.com>
2026-04-17 19:26:12 +0100

    print_pilha implementado folder organizada limites do print_pilha atualizados

--- game/simon_Game/cartasSimon.c
+++ game/simon_Game/cartasSimon.c
@@ -34,55 +34,31 @@ void print_naipe(struct carta c)
 // Recebe uma carta e da print a ela com formato apropriado (valor e naipe com fundo branco)
 void print_carta(struct carta c) 
 {
-    if(c.valor == 0)
-        printf("          ");
-    else
-    {
-        printf("\033[30;47m"); //texto preto (30) fundo branco (47)
-        print_valor(c);
-        print_naipe(c);  
-        printf("\033[0m"); //reseta pro normal
-    } 
+    printf("\033[30;47m"); //texto preto (30) fundo branco (47)
+    print_valor(c);
+    print_naipe(c);  
+    printf("\033[0m"); //reseta pro normal
 }
 
-// Recebe uma pilha e da print dependendo do tamanho. No Simple Simon são 3 pilhas de 8 cartas e o restante decrescente de 7 a 1.
-void print_colunas(struct carta baralho[])
-{
-    printf("    1          2          3          4          5          6          7          8          9          10\n");
+void print_pilhas(Pilhas p, int lim){
+    Pilhas pTemp = p;
+    int linha = 0;
 
-    for (int i = 0; i < 24; i++) 
-    {
-        print_carta(baralho[i]);
-        putchar(' ');
+    while (linha<(lim+1)){
+        
+        for (int i=0; i<10; i++){
+            if(pTemp->numCartas <= linha)
+                printf("          "); //espaco vazio
+            else
+                print_carta(pTemp->pilha[linha]);
+           
+            printf(" ");
+            pTemp = pTemp->prox;
+        }
+        pTemp = p;
+        linha++;
+
+        printf("\n");
     }
-    printf("\n");
 }
-
-// Da print no estado atual do jogo
-void print_game(struct carta baralho[], int stock, struct carta ultima){
-    print_colunas(baralho);
-    printf("Stock: %d ", stock);
-    print_carta(ultima);
-    putchar('\n');
-}
-
-// Pede o numero da jogada no terminal para o player, nao para ate conseguir um numero valido de jogada
-//0 -> sair
-//1 a 7 -> fazer a jogada normal
-//8 -> puxar do stock
-//9 -> restart
-int pedir_jogada()
-{
-    unsigned int jogada_escolhida = 10;
-    
-    printf("Digite o número para sua Jogada: ");
-    scanf("%d", &jogada_escolhida);
-    
-    while(jogada_escolhida > 9 || jogada_escolhida < 0)
-    {
-       printf("Jogada Inválida - Tente Novamente: ");
-       scanf("%d", &jogada_escolhida);
-    }
-
-    return jogada_escolhida;
-}
+ 
