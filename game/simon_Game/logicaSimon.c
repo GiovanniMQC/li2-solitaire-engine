4cc2a33dff51342e538546495828c613c7b49e16
jmar <jmarchon.azv@gmail.com>
2026-04-17 21:00:15 +0100

    acharLimite implementado
    agora o print_pilhas adapta-se ao numero de cartas

--- game/simon_Game/logicaSimon.c
+++ game/simon_Game/logicaSimon.c
@@ -155,116 +155,16 @@ Pilhas mover_cartas(Pilhas *p, int posOrig[], int posDest[], int moveInferiores)
     return *p;
 }
 
-// Recebe um baralho e uma coluna, retorna o index da ultima carta de cima pra baixo da coluna
-int achar_base_coluna(struct carta baralho[], int coluna) 
-{
-    int i = coluna-1;
+int acharLimite(Pilhas p){
+    Pilhas pTemp = p->prox;
+    int maior = p->numCartas;
 
-    //parte da primeira carta da coluna e desce (+7 no indice) pra ver se tem valor ou foi removida com valor 0
-    while(baralho[i].valor != 0)
-    {
-        if(baralho[i+7].valor != 0 && i+7 < 35)
-            i+=7;
-        else
-            return i;
+    for (int i=0; i<9; i++){
+        if (maior < pTemp->numCartas)
+            maior = pTemp->numCartas;
+        
+        pTemp->prox;
     }
 
-    return -1;
+    return maior;
 }
-
-// Inicializa o jogo, atribuindo valor as variaveis iniciais, gerando e dando shuffle em um baralho
-void iniciar_jogo(struct carta baralho[], int *stock, struct carta *ultima, int *gameOver) 
-{
-    cria_baralho(baralho);
-    shuffle_baralho(baralho);
-    
-    *stock = 16;
-    *gameOver = 0;
-    *ultima = baralho[51]; 
-}
-
-// Puxa uma carta do stock para a mao atual, as ultimas 16 cartas correspondem ao stock, sao selecionados consecutivamente ate 35
-void puxar_Stock(struct carta baralho[], int *stock, struct carta *ultima)
-{
-    (*stock)--;
-    *ultima = baralho[35 + *stock];
-}
-
-// Pega na carta atual da mao e na carta correspondente a base da coluna escolhida e     
-// checka se os valores das duas estao em um intervalo de -1/+1
-int valores_Check(struct carta baralho[], struct carta *ultima, int jogadaEscolhida)
-{    
-    int base_idx = achar_base_coluna(baralho, jogadaEscolhida);
-
-        if(ultima->valor == baralho[base_idx].valor-1 || ultima->valor == baralho[base_idx].valor+1)
-        {   
-            return 1;
-        }      
-        else
-            return 0;
-}
-
-// Recebe o n de uma coluna (1 a 7) e verifica se os valores da carta na mao e da carta na base da coluna 
-// estao em um intervalo valido para realizar a jogada, caso contrario a jogada nao e feita
-void jogada_Coluna(struct carta baralho[], int *stock, struct carta *ultima, int jogadaEscolhida)
-{    
-    int base_idx = achar_base_coluna(baralho, jogadaEscolhida);
-
-        if(valores_Check(baralho,&*ultima,jogadaEscolhida))
-        {   
-            *ultima = baralho[base_idx];
-            baralho[base_idx].valor = 0;
-        }      
-        else
-        {
-            printf("Valor das cartas não compatíveis - Tente Novamente: \n");
-            
-        }
-}
-
-// Recebe o baralho, quantidade de cartas do stock e jogadaEscolhida para definir o estado do gameOver dependendo
-// se o player perdeu, ganhou ou apenas saiu do jogo. 
-void checa_gameOver(struct carta baralho[], int *stock, int *gameOver, unsigned int jogadaEscolhida)
-{
-    int naoTemCartas = 1;
-    for(int i = 0; i < 7; i++)
-    {
-        if (baralho[i].valor != 0)
-        {
-            naoTemCartas = 0;
-        }
-    }
-    //SAIR
-    if (naoTemCartas == 1)
-        *gameOver = 2;
-    else if((naoTemCartas == 0 && (*stock) <= 0) || jogadaEscolhida == 0)
-        *gameOver = 1;
-}
-
-// A partir da jogada selecionada, processa a jogada correta para o numero dado
-// void processar_jogada(struct carta baralho[], int *stock, struct carta *ultima, int *gameOver)
-// {
-//     unsigned int jogadaEscolhida = pedir_jogada();
-    
-//     //RESTART
-//     if(jogadaEscolhida == 9)
-//     {
-//         iniciar_jogo(baralho, &*stock,&*ultima,&*gameOver);
-//     }
-
-//     //Jogar
-//     else if(jogadaEscolhida<=7 && jogadaEscolhida>=1)
-//     {
-//         jogada_Coluna(baralho,&*stock,&*ultima,jogadaEscolhida);
-//         return;
-//     }
-    
-//     //Checar se deu gameOver
-//     checa_gameOver(baralho, stock, gameOver, jogadaEscolhida);
-    
-//     //Puxar carta
-//     if (jogadaEscolhida == 8)
-//     {
-//         puxar_Stock(baralho,&*stock,&*ultima);
-//     }
-// }
--- game/simon_Game/main.c
+++ game/simon_Game/main.c
@@ -18,14 +18,14 @@ int main(void)
     int j = 0;
     int capacidade[] = {8,8,8,7,6,5,4,3,2,1};
     Pilhas celula = cria_pilhas(baralho, capacidade, 10);
-    print_pilhas(celula, 8);
+    print_pilhas(celula, acharLimite(celula));
     
     // Movimentação de teste de cartas
     int posOrigem[2] = {5,4};
     int posDestino[2] = {1,8};
     mover_cartas(&celula, posOrigem, posDestino, 1);
-    // printf("\n");
-    print_pilhas(celula, 8);
 
+    print_pilhas(celula, 8);
+    
     return EXIT_SUCCESS;    
 }
