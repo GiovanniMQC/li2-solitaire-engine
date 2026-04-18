f7f7bc4a036b3d6c5f6217b51df82cff087f1df6
jmar <jmarchon.azv@gmail.com>
2026-04-18 17:43:18 +0100

    arquivo de testes CUnit implementado

--- game/simon_Game/logicaSimon.h
+++ game/simon_Game/logicaSimon.h
@@ -1,3 +1,5 @@
+#include "cartasSimon.h"
+
 void cria_baralho(struct carta * baralho);
 void shuffle_baralho(struct carta *baralho);
 int achar_base_coluna(struct carta baralho[], int coluna);
@@ -6,4 +8,3 @@ void puxar_Stock(struct carta baralho[], int *stock, struct carta *ultima);
 int valores_Check(struct carta baralho[], struct carta *ultima, int jogadaEscolhida);
 void jogada_Coluna(struct carta baralho[], int *stock, struct carta *ultima, int jogadaEscolhida);
 void processar_jogada(struct carta baralho[], int *stock, struct carta *ultima, int *gameOver);
-
