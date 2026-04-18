81713f9b970462396d6ee7ea34f62a8d05b95a5a
Ivo Reis <ivo.teixeira.reis@gmail.com>
2026-04-18 19:51:46 +0100

    baixar complexidade do valida jogada

--- game/simon_Game/logicaSimon.c
+++ game/simon_Game/logicaSimon.c
@@ -197,11 +197,8 @@ int acharLimite(Pilhas p){
     return maior;
 }
 
-int valida_jogada(Pilhas *p, int posOrig[], int posDest[])
+int carta_check (Pilhas *p, int posOrig[], int posDest[])
 {
-    if ((posOrig[0] || posDest[0])>=10 || (posDest[0] || posOrig[0] || posDest[1] || posOrig[1])<0)
-        return 1;
-    
     int origCol = posOrig[0];
     int origLin = posOrig[1];
     int destCol = posDest[0];
@@ -224,5 +221,17 @@ int valida_jogada(Pilhas *p, int posOrig[], int posDest[])
         if (!(cartaAverificar.naipe == naipeSelecionado) || (cartaAverificar.valor == (pilhaOrigem->pilha)[i+1].valor-1))
             return 1;
     }
-        mover_cartas(p, posOrig, posDest);
+    return 0;
+}
+
+int valida_jogada(Pilhas *p, int posOrig[], int posDest[])
+{
+    if (posOrig[0]>=10 || posDest[0]>=10 || posDest[0]<0 || posOrig[0]<0 || posDest[1]<0 || posOrig[1]<0)
+        return 1;
+    
+    if (carta_check(p, posOrig, posDest) == 1)
+        return 1;
+
+    mover_cartas(p, posOrig, posDest);
+    return 0;
 }
