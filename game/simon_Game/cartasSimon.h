1f901568840ff6e36d4217331760ed97d3a2246e
Giovanni Carvalho <giovannicavm+github@gmail.com>
2026-05-21 17:07:59 +0100

    Melhor estruturado o .h

--- game/simon_Game/cartasSimon.h
+++ game/simon_Game/cartasSimon.h
@@ -4,61 +4,8 @@ struct carta{
     int valor; 
 };
 
-typedef struct{
-    //Pilhas actuais: tipo da pilha e que cartas
-    //qnts cartas
-    EstadoPilhas estado_pilhas;
-    // n de baralhos
-    int nBaralhos;
-    //que movimentos sao validos
-    MovimentoDef * mov_perm;
-    // descricao dos movimentos automáticos
-    MovimentoDef * auto_movs;
-}EstadoJogo;
-
-typedef struct{
-    char * tipo_pilha; //TAB, DESCARTE
-    size_t qts_pilhas; // tamanho de pilhas
-    PilhaCartas * pilhas; // array de pilhas
-} EstadoPilhas;
-
 struct baralho{
     struct carta cartas[52];
-};
-
-typedef struct{
-    Pilhas celula;
-    MovimentoDef ;
-}EstadoJogo;
-
-typedef struct{
-    char * tipo_origem;
-    char * tipo_destino;
-    enum FlagsMovimento * flags;
-    size_t qts_flags;
-} MovimentoDef;
-
-enum{
-    NAO_HA_RESTRICOES, // * Não há restrições, i.e. é sempre possível mover cartas entre os dois tipos de pilhas.
-    PODE_SER_SEQUENCIAS, // + Pode ser movida uma sequência de cartas.
-    ORDENADAS_DECRESCENTE, // [ As cartas a mover devem estar ordenadas de forma decrescente, por valores consecutivos.
-    ORDENADAS_CRESCENTE, // ] As cartas a mover devem estar ordenadas de forma crescente, por valores consecutivos.
-    VALOR_INFERIOR, // < A carta de topo na sequência (eventualmente singular) a mover deve ter um valor imediatamente inferior à carta de topo no destino.
-    VALOR_SUPERIOR, // > A carta de topo na sequência (eventualmente singular) a mover deve ter um valor imediatamente superior à carta de topo no destino.
-    OU, // ~ Atalho para “< ou > ”“, i.e. o valor deve ser imediatamente superior ou imediatamente inferior.
-    MESMO_NAIPE, // m As cartas a mover devem pertencer ao mesmo naipe
-    TOPO_MESMO_NAIPE, // M A carta de topo na sequência (eventualmente singular) a mover deve pertencer ao mesmo naipe da carta no topo do destino
-    NAIPES_ALTERNADOS, // x As cartas a mover devem ser de naipes alternados, i.e. cada par de cartas consecutivas são de naipes diferentes.
-    NAIPE_DIFERENTE, // X A carta de topo na sequência (eventualmente singular) a mover não deve pertencer ao mesmo naipe da carta no topo do destino.
-    MESMA_COR, // c As cartas a mover devem ter a mesma cor.
-    TOPO_MESMA_COR, // C A carta de topo na sequência (eventualmente singular) a mover deve ter a mesma cor da carta no topo do destino.
-    CORES_ALTERNADAS, // d As cartas a mover devem ter cores alternadas, i.e. cada par de cartas consecutivas têm cores diferentes.
-    TOPO_CORES_ALTERNADAS, // D A carta de topo na sequência (eventualmente singular) a mover não deve ter a mesma cor da carta no topo do destino.
-    PILHA_VAZIA, // V A pilha de destino deve estar vazia.
-    TOPO_AS, // a A carta de topo na sequência (eventualmente singular) a mover deve ser um Ás.
-    FUNDO_AS, // A A carta de fundo na sequência (eventualmente singular) a mover deve ser um Ás.
-    TOPO_REI, // k A carta de topo na sequência (eventualmente singular) a mover deve ser um Rei.
-    FUNDO_REI // K A carta de fundo na sequência (eventualmente singular) a mover deve ser um Rei.
 };
 
 typedef struct celula // guarda a lista ligada de pilhas
@@ -68,6 +15,57 @@ typedef struct celula // guarda a lista ligada de pilhas
     struct carta *pilha; //apontador para a primeira carta
     struct celula *prox; //apontador para a proxima celula (proxima pilha)
 } *Pilhas;
+
+typedef struct {
+    char *tipo_pilha; // Ex: "TAB", "DESCARTE"
+    int qts_pilhas;   // Quantidade de pilhas alocadas
+    Pilhas pilhas;    // Apontador para a tua lista ligada atual
+} EstadoPilhas;
+
+// Em vez de bits, usamos números normais (0 para Falso, 1 para Verdadeiro)
+typedef struct {
+    int nao_ha_restricoes;      // *
+    int pode_ser_sequencias;    // +
+    int ordenadas_decrescente;  // [
+    int ordenadas_crescente;    // ]
+    int valor_inferior;         // <
+    int valor_superior;         // >
+    int ou;                     // ~
+    int mesmo_naipe;            // m
+    int topo_mesmo_naipe;       // M
+    int naipes_alternados;      // x
+    int naipe_diferente;        // X
+    int mesma_cor;              // c
+    int topo_mesma_cor;         // C
+    int cores_alternadas;       // d
+    int topo_cores_alternadas;  // D
+    int pilha_vazia;            // V
+    int topo_as;                // a
+    int fundo_as;               // A
+    int topo_rei;               // k
+    int fundo_rei;              // K
+} FlagsMovimento;
+
+typedef struct {
+    char *tipo_origem;    // Ex: "TAB"
+    char *tipo_destino;   // Ex: "FUNDACAO"
+    FlagsMovimento flags; // Estrutura com as regras (apenas 0s ou 1s)
+} MovimentoDef;
+
+// A estrutura principal que guarda toda a informação de uma Paciência lida do ficheiro
+typedef struct {
+    char *nome_paciencia; // Nome retirado do ficheiro
+    int nBaralhos;        // Quantidade de baralhos
+    
+    EstadoPilhas *estado_pilhas; // Array dinâmico com os tipos de pilhas
+    int qts_tipos_pilhas;
+    
+    MovimentoDef *mov_perm;      // Array dinâmico de movimentos permitidos
+    int qts_mov_perm;
+    
+    MovimentoDef *auto_movs;     // Array dinâmico de movimentos automáticos
+    int qts_auto_movs;
+} EstadoJogo;
 
 void print_valor(struct carta c);
 void print_naipe(struct carta c);
