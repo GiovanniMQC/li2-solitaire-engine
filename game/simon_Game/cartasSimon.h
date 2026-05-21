5619c5ef1c37059af1d1d52fa31b3c2e7658efbc
Giovanni Carvalho <giovannicavm+github@gmail.com>
2026-05-21 18:01:40 +0100

    Voltando para versão com enum

--- game/simon_Game/cartasSimon.h
+++ game/simon_Game/cartasSimon.h
@@ -16,40 +16,34 @@ typedef struct celula // guarda a lista ligada de pilhas
     struct celula *prox; //apontador para a proxima celula (proxima pilha)
 } *Pilhas;
 
-typedef struct {
-    char *tipo_pilha; // Ex: "TAB", "DESCARTE"
-    int qts_pilhas;   // Quantidade de pilhas alocadas
-    Pilhas pilhas;    // Apontador para a tua lista ligada atual
-} EstadoPilhas;
-
-// Em vez de bits, usamos números normais (0 para Falso, 1 para Verdadeiro)
-typedef struct {
-    int nao_ha_restricoes;      // *
-    int pode_ser_sequencias;    // +
-    int ordenadas_decrescente;  // [
-    int ordenadas_crescente;    // ]
-    int valor_inferior;         // <
-    int valor_superior;         // >
-    int ou;                     // ~
-    int mesmo_naipe;            // m
-    int topo_mesmo_naipe;       // M
-    int naipes_alternados;      // x
-    int naipe_diferente;        // X
-    int mesma_cor;              // c
-    int topo_mesma_cor;         // C
-    int cores_alternadas;       // d
-    int topo_cores_alternadas;  // D
-    int pilha_vazia;            // V
-    int topo_as;                // a
-    int fundo_as;               // A
-    int topo_rei;               // k
-    int fundo_rei;              // K
+typedef enum {
+    NAO_HA_RESTRICOES,      // *
+    PODE_SER_SEQUENCIAS,    // +
+    ORDENADAS_DECRESCENTE,  // [
+    ORDENADAS_CRESCENTE,    // ]
+    VALOR_INFERIOR,         // <
+    VALOR_SUPERIOR,         // >
+    OU,                     // ~
+    MESMO_NAIPE,            // m
+    TOPO_MESMO_NAIPE,       // M
+    NAIPES_ALTERNADOS,      // x
+    NAIPE_DIFERENTE,        // X
+    MESMA_COR,              // c
+    TOPO_MESMA_COR,         // C
+    CORES_ALTERNADAS,       // d
+    TOPO_CORES_ALTERNADAS,  // D
+    PILHA_VAZIA,            // V
+    TOPO_AS,                // a
+    FUNDO_AS,               // A
+    TOPO_REI,               // k
+    FUNDO_REI               // K
 } FlagsMovimento;
 
 typedef struct {
-    char *tipo_origem;    // Ex: "TAB"
-    char *tipo_destino;   // Ex: "FUNDACAO"
-    FlagsMovimento flags; // Estrutura com as regras (apenas 0s ou 1s)
+    char *tipo_origem;     // Ex: "TAB"
+    char *tipo_destino;    // Ex: "FUNDACAO"
+    FlagsMovimento *flags; // Array dinâmico de enums (guarda as múltiplas regras do movimento)
+    int qts_flags;         // Tamanho do array
 } MovimentoDef;
 
 // A estrutura principal que guarda toda a informação de uma Paciência lida do ficheiro
@@ -57,8 +51,8 @@ typedef struct {
     char *nome_paciencia; // Nome retirado do ficheiro
     int nBaralhos;        // Quantidade de baralhos
     
-    EstadoPilhas *estado_pilhas; // Array dinâmico com os tipos de pilhas
-    int qts_tipos_pilhas;
+    Pilhas pilhas;        // Lista ligada com todas as pilhas do jogo
+    int qts_pilhas;       // Quantidade total de pilhas instanciadas
     
     MovimentoDef *mov_perm;      // Array dinâmico de movimentos permitidos
     int qts_mov_perm;
