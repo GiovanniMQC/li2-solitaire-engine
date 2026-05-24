f834ce9f48cf9f2858609aea9448e7a994abd6ac
jmar <jmarchon.azv@gmail.com>
2026-05-24 18:50:12 +0100

    testes cunit implementados

--- game/golf_Game/logica.h
+++ game/golf_Game/logica.h
@@ -1,3 +1,10 @@
+#include "cartas.h"
+#include "logica.h"
+#include "estadoJogoPrototipo.h" 
+
+int valida_jogada(Pilha p_falsa, int posOrigem, int posDestino);
+void corrige_seq_cartas(Pilha *p, int valor);
+int check_gameOver(Tabuleiro *t);
 void cria_baralho(struct carta * baralho);
 void shuffle_baralho(struct carta *baralho);
 int achar_base_coluna(struct carta baralho[], int coluna);
@@ -6,4 +13,7 @@ void puxar_Stock(struct carta baralho[], int *stock, struct carta *ultima);
 int valores_Check(struct carta baralho[], struct carta *ultima, int jogadaEscolhida);
 void jogada_Coluna(struct carta baralho[], int *stock, struct carta *ultima, int jogadaEscolhida);
 void processar_jogada(struct carta baralho[], int *stock, struct carta *ultima, int *gameOver);
-
+int check_gameOver(Tabuleiro *t);
+int carta_check(Pilha *orig, Pilha *dest, struct carta c1, struct carta c2, int valor, int naipe); // Ajusta os tipos se forem diferentes no teu logica.c
+int sequencias(Pilha p);
+int existe_jogadaValida(Pilha *p_unica);
